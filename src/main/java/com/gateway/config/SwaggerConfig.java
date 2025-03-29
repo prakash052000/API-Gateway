aa8af2fedf401111606ab4ac6c8ffabd7988e2ca
prakash052000 <prakashmaniraj07@gmail.com>
2025-03-30 00:47:20 +0530

    pom file changes with sequrity

--- src/main/java/com/gateway/config/SwaggerConfig.java
+++ src/main/java/com/gateway/config/SwaggerConfig.java
@@ -1,11 +1,11 @@
 package com.gateway.config;
-import org.springdoc.core.models.GroupedOpenApi;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Configuration
 public class SwaggerConfig {
